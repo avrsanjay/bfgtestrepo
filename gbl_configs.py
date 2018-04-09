1d98c7c9effb8f5ff0ce72dce5b50fe247fddbe6
Sanjay Akella <avrsanjay@users.noreply.github.com>
2018-04-09 14:22:34 -0400

    Create gbl_configs.py

---
+++ gbl_configs.py
@@ -0,0 +1,9 @@
+s3_creds = {
+    'bucket_url'        : {'incremental' : '***REMOVED***',
+                           'historical'  : '***REMOVED***'},
+    's3_access_key'     : "***REMOVED***",
+    's3_secret_key'     : "***REMOVED***",
+    's3_proxy_host'     : "***REMOVED***",
+    's3_proxy_port'     : "80",
+    'num_mappers'       : "200"
+}
